2dca29a1437899dd081aa9dc8ace2c03530442cf
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-06 17:12:02 +0530

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ["error", "windows"] */
 import './App.css';
 import Calculator from './components/calculator';
 
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ["error", "windows"] */
 import './styles/calculator.css';
 
 const Calculator = () => (
