2f786ad6179b015bb9350cdff8b618bd62164aeb
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-13 21:18:17 +0530

    add styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import Home from './components/Home';
 
 function App() {
+  const [activeLink, setActiveLink] = useState('/'); // Set initial active link to "/"
   return (
     <div className="App">
+      <nav>
+        <h1 className="title">Math Magicians</h1>
+        <ul className="links-container">
+          <li>
+            <Link
+              to="/"
+              onClick={() => setActiveLink('/')}
+              className={activeLink === '/' ? 'active' : ''}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="vl" />
+          <li>
+            <Link
+              to="/quote"
+              onClick={() => setActiveLink('/quote')}
+              className={activeLink === '/quote' ? 'active' : ''}
+            >
+              Quote
+            </Link>
+          </li>
+          <li className="vl" />
+          <li>
+            <Link
+              to="/calculator"
+              onClick={() => setActiveLink('/calculator')}
+              className={activeLink === '/calculator' ? 'active' : ''}
+            >
+              Calculator
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quote" element={<Quote />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,21 @@
+import './styles/home.css';
+
 const Home = () => (
   <div className="home">
-    <h2>Homepage</h2>
+    <section className="content">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Math magicians is a website for all fans of mathematics. It is a Single
+        Page App (SPA) that allows users to:
+      </p>
+      <ul>
+        <li>☑️Read a random math-related quote.</li>
+        <li>☑️Make simple calculations.</li>
+      </ul>
+    </section>
+    <section className="img-container">
+      <img src="https://i.gifer.com/G0yR.gif" alt="math" />
+    </section>
   </div>
 );
 export default Home;
