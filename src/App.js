df283459d1d148711462c795869ee08890477742
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-13 12:25:52 +0530

    add routers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
@@ -6,9 +8,11 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Quote />
-      <Calculator />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
     </div>
   );
 }
